70dd875886ceb3c4424599cf207838381178c849
Arpit-Jaiswal20 <arpitjaiswalaj9@gmail.com>
2025-02-21 23:54:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+let button = document.getElementById("removeBtn");
+        let select = document.getElementById("colorSelect");
+
+        // Add event listener to button
+        button.addEventListener("click", function() {
+            let selectedValue = select.value;  
+            let selectedIndex = select.selectedIndex; 
+
+            if (selectedIndex !== -1) {  // Ensure an option is selected
+                // alert("Selected color: " + selectedValue); // Show selected value
+                select.remove(selectedIndex); // Remove selected option
+            }
+        });
